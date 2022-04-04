3f4cd2d968d90f46d10ba2b8def851fb34222b95
Farkad Adnan <35774039+FarkadAdnan@users.noreply.github.com>
2022-04-04 05:33:34 +0300

    Add files via upload

---
+++ code_experience_80/code_experience_80.ino
@@ -0,0 +1,28 @@
+/*
+ By:Farkad Adnan
+ E-mail: farkad.hpfa95@gmail.com
+ inst : farkadadnan
+ #farkadadnan , #farkad_adnan , فرقد عدنان#
+ FaceBook: كتاب عالم الاردوينو
+ inst : arduinobook
+ #كتاب_عالم_الاردوينو  #كتاب_عالم_الآردوينو 
+ */
+const int GSR=A0;
+int sensorValue=0;
+int gsr_average=0;
+ 
+void setup(){
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  long sum=0;
+  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+      {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+      }
+   gsr_average = sum/10;
+   Serial.println(gsr_average);
+}
